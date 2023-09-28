cbc77fbb342aef687b4389889c9378dfe1ed0c46
Karl George <karlgdev@gmail.com>
2023-09-28 15:11:32 +0200

    add resource card component

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -1,13 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
+
+export const revalidate = 900;
 
 interface ResourceCardProps {
   id: string;
@@ -27,8 +22,8 @@ function ResourceCard({
   return (
     <Card className='w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]'>
       <Link href={`/resource/${id}`}>
-        <CardHeader>
-          <div>
+        <CardHeader className='flex-center flex-col gap-2.5 !p-0'>
+          <div className='h-fit w-full'>
             <Image
               src={image}
               className='h-full rounded-md object-contain'
@@ -37,16 +32,24 @@ function ResourceCard({
               alt='title'
             />
           </div>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle className='text-white paragraph-semibold line-clamp-1 w-full text-left'>
+            {title}
+          </CardTitle>
         </CardHeader>
       </Link>
-      <CardContent>
-        <p>Card Content</p>
+      <CardContent className='flex-between mt-4 p-0'>
+        <div className='flex-center body-medium gap-1.5 text-white'>
+          <Image src='/downloads.svg' width={20} height={20} alt='download' />
+          {downloadNumber}
+        </div>
+        <Link
+          href={`/resource/${id}`}
+          className='flex-center text-gradient_purple-blue body-semibold gap-1.5'
+        >
+          Download Now
+          <Image src='/arrow-blue.svg' width={13} height={10} alt='arrow' />
+        </Link>
       </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
     </Card>
   );
 }
