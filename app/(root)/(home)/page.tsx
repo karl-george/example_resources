9012bcdab4ed592ba2f2f637812a73f8ba88c743
Karl George <karlgdev@gmail.com>
2023-09-28 12:33:31 +0200

    add search and filters to main page

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,18 @@
+import Filters from '@/components/Filters';
+import SearchForm from '@/components/SearchForm';
+
+export default function Home() {
+  return (
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
+      <section className='nav-padding w-full'>
+        <div className='flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
+          <h1 className='sm:heading1 heading2 mb-6 text-center text-white'>
+            JavaScript Resources
+          </h1>
+        </div>
+        <SearchForm />
+      </section>
+      <Filters />
+    </main>
+  );
+}
