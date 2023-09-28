c8da5daecdc4c5bee6b9b5744dcbc73236747190
Karl George <karlgdev@gmail.com>
2023-09-28 13:03:53 +0200

    add showing resources

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,13 @@
 import Filters from '@/components/Filters';
+import ResourceCard from '@/components/ResourceCard';
 import SearchForm from '@/components/SearchForm';
+import { getResources } from '@/sanity/actions';
 
-export default function Home() {
+export default async function Home() {
+  const resources = await getResources({ query: '', category: '', page: '1' });
+
+  console.log(resources);
+
   return (
     <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
       <section className='nav-padding w-full'>
@@ -13,6 +19,26 @@ export default function Home() {
         <SearchForm />
       </section>
       <Filters />
+
+      <section className='flex-center mt-6 w-full flex-col sm:mt-20'>
+        Header
+        <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
+          {resources.length > 0 ? (
+            resources.map((resource: any) => (
+              <ResourceCard
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                slug={resource.slug}
+                downloadNumber={resource.views}
+              />
+            ))
+          ) : (
+            <p className='body-regular text-white-400'>No resources found</p>
+          )}
+        </div>
+      </section>
     </main>
   );
 }
