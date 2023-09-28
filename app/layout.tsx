136451f2c60f4fe9fca5898571a35864c01e133f
Karl George <karlgdev@gmail.com>
2023-09-28 11:23:02 +0200

    adding black background

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='min-h-screen bg-black-100 font-poppins'>{children}</body>
     </html>
   );
 }
