b8c8d3093b073573239262203baeee2d01065a35
Karl George <karlgdev@gmail.com>
2023-09-28 15:34:25 +0200

    add debounce search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,13 +1,40 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Input } from './ui/input';
+import { formUrlQuery } from '@/sanity/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 interface SearchFormProps {}
 
 function SearchForm({}: SearchFormProps) {
   const [search, setSearch] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // Create a debounce function for the search field
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = '';
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'query',
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['query'],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
 
   return (
     <form className='flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5'>
