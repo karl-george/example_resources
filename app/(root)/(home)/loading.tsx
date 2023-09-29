a68de735c93e049b570a502e8eee544b16d6e2c8
Karl George <karlgdev@gmail.com>
2023-09-29 11:32:36 +0200

    fix loading colours

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -9,12 +9,12 @@ function loading({}: loadingProps) {
         <Skeleton className='w-full h-[274px] rounded-lg bg-red-500' />
       </section>
       <section className='mt-6 w-full flex-col flex sm:mt-20'>
-        <Skeleton className='h-10 w-56 bg-red-500' />
+        <Skeleton className='h-10 w-56 bg-black-200/40' />
         <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
-          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-red-500' />
-          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-red-500' />
-          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-red-500' />
-          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-red-500' />
+          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-black-200/40' />
+          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-black-200/40' />
+          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-black-200/40' />
+          <Skeleton className='h-[440px] w-full sm:w-[356px] bg-black-200/40' />
         </div>
       </section>
     </main>
